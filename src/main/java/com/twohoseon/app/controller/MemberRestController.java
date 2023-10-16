145df31e8b514d66ad2b3fdbf2cdeaa4f023093d
Pin <ppyj48625@naver.com>
2023-10-16 20:53:36 +0900

    feat: MemberRestController 추가 #8

---
+++ src/main/java/com/twohoseon/app/controller/MemberRestController.java
@@ -0,0 +1,47 @@
+package com.twohoseon.app.controller;
+
+import com.twohoseon.app.dto.ProfileRequestDTO;
+import com.twohoseon.app.dto.ResultDTO;
+import com.twohoseon.app.enums.StatusEnum;
+import com.twohoseon.app.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberRestController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/profiles")
+    public ResponseEntity<ResultDTO> setMemberInfo(@RequestBody ProfileRequestDTO profileRequestDTO) {
+        memberService.setUserProfile(profileRequestDTO);
+
+        ResultDTO resultDTO = ResultDTO.builder()
+                .status(StatusEnum.OK)
+                .message("success")
+                .build();
+
+        return ResponseEntity.ok(resultDTO);
+    }
+
+    @PostMapping("/api/profiles/isValidNickname")
+    public ResponseEntity<ResultDTO> checkNicknameDuplicate(String userNickname) {
+
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("isExist", memberService.validateDuplicateMember(userNickname));
+        ResultDTO resultDTO = ResultDTO.builder()
+                .status(StatusEnum.OK)
+                .message("생공")
+                .data(result)
+                .build();
+
+        return ResponseEntity.ok(resultDTO);
+    }
+}
