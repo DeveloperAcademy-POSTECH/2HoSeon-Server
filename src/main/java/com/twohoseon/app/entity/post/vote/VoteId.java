e76c72ebf3c174c230ea5dece0166d1fb21d8285
migusdn <migusdn@gmail.com>
2023-10-20 15:55:09 +0900

    feat: Vote 관련 entity, repo 등 정의 #14

---
+++ src/main/java/com/twohoseon/app/entity/post/vote/VoteId.java
@@ -0,0 +1,53 @@
+package com.twohoseon.app.entity.post.vote;
+
+import com.twohoseon.app.entity.member.Member;
+import com.twohoseon.app.entity.post.Post;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.io.Serializable;
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : VoteId
+ * @date : 10/19/23 3:28 PM
+ * @modifyed : $
+ **/
+@Embeddable
+@Builder
+@AllArgsConstructor
+//@EqualsAndHashCode
+public class VoteId implements Serializable {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voter_id")
+    private Member voter;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VoteId)) return false;
+        VoteId voteId = (VoteId) o;
+        return post.equals(voteId.post) && voter.equals(voteId.voter);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    protected VoteId() {
+    }
+    // getters, setters, equals, hashCode 메서드가 필요합니다.
+}
+
