7f68d0f5b32fc05818be2462fe3d69f115dfdfbf
migusdn <migusdn@gmail.com>
2023-10-21 06:31:45 +0900

    feat: add exception handler, custom exception class

---
+++ src/main/java/com/twohoseon/app/enums/ErrorCode.java
@@ -0,0 +1,133 @@
+package com.twohoseon.app.enums;
+
+import lombok.Getter;
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : ErrorCode
+ * @date : 2023/10/21 4:32 PM
+ * @modifyed : $
+ * <p>
+ * [공통 코드] API 통신에 대한 '에러 코드'를 Enum 형태로 관리를 한다.
+ * Global Error CodeList : 전역으로 발생하는 에러코드를 관리한다.
+ * Custom Error CodeList : 추가된 에러코드
+ * Error Code Constructor : 에러코드를 직접적으로 사용하기 위한 생성자를 구성한다.
+ */
+@Getter
+public enum ErrorCode {
+
+    /**
+     * ******************************* Global Error CodeList ***************************************
+     * HTTP Status Code
+     * 400 : Bad Request
+     * 401 : Unauthorized
+     * 403 : Forbidden
+     * 404 : Not Found
+     * 500 : Internal Server Error
+     * *********************************************************************************************
+     */
+    // 잘못된 서버 요청
+    BAD_REQUEST_ERROR(400, "G001", "Bad Request Exception"),
+
+    // @RequestBody 데이터 미 존재
+    REQUEST_BODY_MISSING_ERROR(400, "G002", "Required request body is missing"),
+
+    // 유효하지 않은 타입
+    INVALID_TYPE_VALUE(400, "G003", " Invalid Type Value"),
+
+    // Request Parameter 로 데이터가 전달되지 않을 경우
+    MISSING_REQUEST_PARAMETER_ERROR(400, "G004", "Missing Servlet RequestParameter Exception"),
+
+    // 입력/출력 값이 유효하지 않음
+    IO_ERROR(400, "G005", "I/O Exception"),
+
+    // com.google.gson JSON 파싱 실패
+    JSON_PARSE_ERROR(400, "G006", "JsonParseException"),
+
+    // com.fasterxml.jackson.core Processing Error
+    JACKSON_PROCESS_ERROR(400, "G007", "com.fasterxml.jackson.core Exception"),
+
+
+    // 권한이 없음
+    FORBIDDEN_ERROR(403, "G010", "Forbidden Exception"),
+
+    // 서버로 요청한 리소스가 존재하지 않음
+    NOT_FOUND_ERROR(404, "G011", "Not Found Exception"),
+
+    // NULL Point Exception 발생
+    NULL_POINT_ERROR(404, "G012", "Null Point Exception"),
+
+    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
+    NOT_VALID_ERROR(404, "G013", "handle Validation Exception"),
+
+    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
+    NOT_VALID_HEADER_ERROR(404, "G014", "Header에 데이터가 존재하지 않는 경우 "),
+
+    // 서버가 처리 할 방법을 모르는 경우 발생
+    INTERNAL_SERVER_ERROR(500, "G999", "Internal Server Error Exception"),
+
+
+    /**
+     * ******************************* Custom Error CodeList ***************************************
+     */
+
+    // 회원이 존재하지 않음
+    MEMBER_NOT_FOUND_ERROR(404, "E001", "Member Not Found Exception"),
+
+    // 게시글이 존재하지 않음
+    POST_NOT_FOUND_ERROR(404, "E002", "Post Not Found Exception"),
+
+    // 댓글이 존재하지 않음
+    COMMENT_NOT_FOUND_ERROR(404, "E003", "Comment Not Found Exception"),
+
+    // JWT 서명이 유효하지 않음
+    INVALID_SIGNATURE_ERROR(401, "E004", "Invalid JWT Signature"),
+
+    // JWT 형식이 올바르지 않음
+    MALFORMED_TOKEN_ERROR(401, "E005", "Invalid JWT Token"),
+
+    // JWT 토큰이 만료됨
+    EXPIRED_TOKEN_ERROR(401, "E006", "Expired JWT Token"),
+
+    // 지원하지 않는 JWT 토큰
+    UNSUPPORTED_TOKEN_ERROR(401, "E007", "Unsupported JWT Token"),
+
+    // JWT 클레임 문자열이 비어 있음
+    EMPTY_CLAIMS_ERROR(400, "E008", "JWT Claims String is Empty"),
+
+    // 회원 가입이 완료되지 않음
+    NOT_COMPLETED_SIGNUP_ERROR(400, "E009", "Not Completed SignUp Exception"),
+
+    // Transaction Insert Error
+    INSERT_ERROR(200, "E010", "Insert Transaction Error Exception"),
+
+    // Transaction Update Error
+    UPDATE_ERROR(200, "E011", "Update Transaction Error Exception"),
+
+    // Transaction Delete Error
+    DELETE_ERROR(200, "E012", "Delete Transaction Error Exception"),
+
+    // 닉네임이 중복됨
+    NICKNAME_DUPLICATE_ERROR(409, "E013", "Nickname is already in use");
+
+    /**
+     * ******************************* Error Code Constructor ***************************************
+     */
+    // 에러 코드의 '코드 상태'을 반환한다.
+    private final int status;
+
+    // 에러 코드의 '코드간 구분 값'을 반환한다.
+    private final String divisionCode;
+
+    // 에러 코드의 '코드 메시지'을 반환한다.
+    private final String message;
+
+    // 생성자 구성
+    ErrorCode(final int status, final String divisionCode, final String message) {
+        this.status = status;
+        this.divisionCode = divisionCode;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/twohoseon/app/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,273 @@
+package com.twohoseon.app.exception.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.twohoseon.app.dto.ErrorResponse;
+import com.twohoseon.app.enums.ErrorCode;
+import com.twohoseon.app.exception.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.io.IOException;
+
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : GlobalExceptionHandler
+ * @date : 2023/10/10 5:16 AM
+ * @modifyed : $
+ * <p>
+ * Controller 내에서 발생하는 Exception 대해서 Catch 하여 응답값(Response)을 보내주는 기능을 수행함.
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final HttpStatus HTTP_STATUS_OK = HttpStatus.OK;
+
+    /**
+     * [Exception] API 호출 시 '객체' 혹은 '파라미터' 데이터 값이 유효하지 않은 경우
+     *
+     * @param ex MethodArgumentNotValidException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("handleMethodArgumentNotValidException", ex);
+        BindingResult bindingResult = ex.getBindingResult();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            stringBuilder.append(fieldError.getField()).append(":");
+            stringBuilder.append(fieldError.getDefaultMessage());
+            stringBuilder.append(", ");
+        }
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_VALID_ERROR, String.valueOf(stringBuilder));
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+    /**
+     * [Exception] API 호출 시 'Header' 내에 데이터 값이 유효하지 않은 경우
+     *
+     * @param ex MissingRequestHeaderException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    protected ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(MissingRequestHeaderException ex) {
+        log.error("MissingRequestHeaderException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.REQUEST_BODY_MISSING_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+    /**
+     * [Exception] 클라이언트에서 Body로 '객체' 데이터가 넘어오지 않았을 경우
+     *
+     * @param ex HttpMessageNotReadableException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex) {
+        log.error("HttpMessageNotReadableException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.REQUEST_BODY_MISSING_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * [Exception] 클라이언트에서 request로 '파라미터로' 데이터가 넘어오지 않았을 경우
+     *
+     * @param ex MissingServletRequestParameterException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected ResponseEntity<ErrorResponse> handleMissingRequestHeaderExceptionException(
+            MissingServletRequestParameterException ex) {
+        log.error("handleMissingServletRequestParameterException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.MISSING_REQUEST_PARAMETER_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+    /**
+     * [Exception] 잘못된 서버 요청일 경우 발생한 경우
+     *
+     * @param e HttpClientErrorException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    protected ResponseEntity<ErrorResponse> handleBadRequestException(HttpClientErrorException e) {
+        log.error("HttpClientErrorException.BadRequest", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.BAD_REQUEST_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+
+    /**
+     * [Exception] 잘못된 주소로 요청 한 경우
+     *
+     * @param e NoHandlerFoundException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNoHandlerFoundExceptionException(NoHandlerFoundException e) {
+        log.error("handleNoHandlerFoundExceptionException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+
+    /**
+     * [Exception] NULL 값이 발생한 경우
+     *
+     * @param e NullPointerException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException e) {
+        log.error("handleNullPointerException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.NULL_POINT_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+    /**
+     * Input / Output 내에서 발생한 경우
+     *
+     * @param ex IOException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(IOException.class)
+    protected ResponseEntity<ErrorResponse> handleIOException(IOException ex) {
+        log.error("handleIOException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.IO_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+
+    /**
+     * com.fasterxml.jackson.core 내에 Exception 발생하는 경우
+     *
+     * @param ex JsonProcessingException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(JsonProcessingException.class)
+    protected ResponseEntity<ErrorResponse> handleJsonProcessingException(JsonProcessingException ex) {
+        log.error("handleJsonProcessingException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.REQUEST_BODY_MISSING_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+
+    /**
+     * [Exception] MemberNotFoundException 발생하는 경우
+     *
+     * @param ex MemberNotFoundException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(MemberNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleMemberNotFoundException(MemberNotFoundException ex) {
+        log.error("handleMemberNotFoundException", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.MEMBER_NOT_FOUND_ERROR);
+    }
+
+    /**
+     * [Exception] IncompleteRegistrationException 발생하는 경우
+     *
+     * @param ex IncompleteRegistrationException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(IncompleteRegistrationException.class)
+    protected ResponseEntity<ErrorResponse> handleIncompleteRegistrationException(IncompleteRegistrationException ex) {
+        log.error("handleIncompleteRegistrationException", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.NOT_COMPLETED_SIGNUP_ERROR);
+    }
+
+    /**
+     * [Exception] PostNotFoundException 발생하는 경우
+     *
+     * @param ex PostNotFoundException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(PostNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handlePostNotFoundException(PostNotFoundException ex) {
+        log.error("handlePostNotFoundException", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.POST_NOT_FOUND_ERROR);
+    }
+
+    /**
+     * [Exception] CommentNotFoundException 발생하는 경우
+     *
+     * @param ex CommentNotFoundException
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(CommentNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException ex) {
+        log.error("handleCommentNotFoundException", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.COMMENT_NOT_FOUND_ERROR);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    protected ResponseEntity<ErrorResponse> handleSignatureException(SignatureException ex) {
+        log.error("handleSignatureException: Invalid JWT signature", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.INVALID_SIGNATURE_ERROR);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    protected ResponseEntity<ErrorResponse> handleMalformedJwtException(MalformedJwtException ex) {
+        log.error("handleMalformedJwtException: Invalid JWT token", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.MALFORMED_TOKEN_ERROR);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    protected ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
+        log.error("handleExpiredJwtException: Expired JWT token", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.EXPIRED_TOKEN_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    protected ResponseEntity<ErrorResponse> handleUnsupportedJwtException(UnsupportedJwtException ex) {
+        log.error("handleUnsupportedJwtException: Unsupported JWT token", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.UNSUPPORTED_TOKEN_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("handleIllegalArgumentException: JWT claims string is empty", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.EMPTY_CLAIMS_ERROR);
+    }
+
+    @ExceptionHandler(NicknameDuplicateException.class)
+    protected ResponseEntity<ErrorResponse> handleNicknameDuplicateException(NicknameDuplicateException ex) {
+        log.error("handleNicknameDuplicateException: Nickname is already in use", ex);
+        return ErrorResponse.toResponseEntity(ErrorCode.NICKNAME_DUPLICATE_ERROR);
+    }
+
+
+    // ==================================================================================================================
+
+    /**
+     * [Exception] 모든 Exception 경우 발생
+     *
+     * @param ex Exception
+     * @return ResponseEntity<ErrorResponse>
+     */
+    @ExceptionHandler(Exception.class)
+    protected final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
+        log.error("Exception", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, ex.getMessage());
+        return new ResponseEntity<>(response, HTTP_STATUS_OK);
+    }
+}
