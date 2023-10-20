7c31b8c49cbdde8b59971524544c63f91df9a809
migusdn <migusdn@gmail.com>
2023-10-20 15:58:44 +0900

    chore: SwaggerConfig 변경 #14

--- src/main/java/com/twohoseon/app/config/SwaggerConfig.java
+++ src/main/java/com/twohoseon/app/config/SwaggerConfig.java
@@ -5,9 +5,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 /**
@@ -21,16 +25,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
     public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
         Info info = new Info().title("Demo API").version(appVersion)
-                .description("임시 API")
+                .description("임시 API 아래 Authorize를 클릭하고 Bearer에 토큰을 입력하세요.")
                 .termsOfService("http://swagger.io/terms/")
                 .contact(new Contact().name("hyunwoopark").email("migusdn@gmail.com"))
                 .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement))
                 .info(info);
     }
 }
