7267e8be45fa85fbcb9d1170f1713d26f98bcf0e
migusdn <migusdn@gmail.com>
2023-10-22 00:12:59 +0900

    feat: add nickname valid check request class

---
+++ src/main/java/com/twohoseon/app/dto/request/NicknameValidCheckRequest.java
@@ -0,0 +1,16 @@
+package com.twohoseon.app.dto.request;
+
+import lombok.Getter;
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : NicknameValidCheckRequest
+ * @date : 10/21/23 9:24 PM
+ * @modifyed : $
+ **/
+@Getter
+public class NicknameValidCheckRequest {
+    private String userNickname;
+}
