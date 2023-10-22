f033430d8a6635350d934f61e099779e63b3b2ee
migusdn <migusdn@gmail.com>
2023-10-23 04:47:31 +0900

    feat: post category type Enum 추가

---
+++ src/main/java/com/twohoseon/app/enums/PostCategoryType.java
@@ -0,0 +1,13 @@
+package com.twohoseon.app.enums;
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : PostCategory
+ * @date : 10/23/23 1:08 AM
+ * @modifyed : $
+ **/
+public enum PostCategoryType {
+    PURCHASE_CONSIDERATION, ACTION_CONSIDERATION, FOOD_CONSIDERATION
+}
