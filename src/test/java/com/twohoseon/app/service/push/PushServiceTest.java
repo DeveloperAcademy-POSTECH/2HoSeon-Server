61c4114fb9fbdc8c12c952231e3822817360a230
migusdn <migusdn@gmail.com>
2023-11-01 20:23:56 +0900

    feat: app내 뷰 전환을 위한 notification send Test 수정

--- src/test/java/com/twohoseon/app/service/push/PushServiceTest.java
+++ src/test/java/com/twohoseon/app/service/push/PushServiceTest.java
@@ -2,16 +2,13 @@ package com.twohoseon.app.service.push;
 
 import com.eatthepath.pushy.apns.ApnsClient;
 import com.eatthepath.pushy.apns.PushNotificationResponse;
-import com.eatthepath.pushy.apns.util.SimpleApnsPayloadBuilder;
 import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
 import com.eatthepath.pushy.apns.util.TokenUtil;
+import com.twohoseon.app.dto.apns.CustomApnsPayloadBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,15 +28,16 @@ class PushServiceTest {
     private ApnsClient apnsClient;
 
     @Test
-    public void pushTest() throws IOException, NoSuchAlgorithmException, InvalidKeyException, ExecutionException, InterruptedException {
+    public void pushTest() throws ExecutionException, InterruptedException {
 
-        final String deviceToken = "80df8cbcc1a556d6cef530c6eacfb0b3e33e54e26010328769b2f83ee3a7d445645c94a04de6ba31b78a0c27cd0de712e1f06e07df284635ab9c51972e7130b28c41f3c2240e9c98a379f10e44ed1574"; // Replace with the actual device token
+        final String deviceToken = "c3af1037992c052cf9f9ffe43f266b53e536f3f5f453c3eb335064a6e126429e"; // Replace with the actual device token
 
         final SimpleApnsPushNotification pushNotification = new SimpleApnsPushNotification(
                 TokenUtil.sanitizeTokenString(deviceToken),
                 "com.twohosun.TwoHoSun",
-                new SimpleApnsPayloadBuilder()
-                        .setAlertBody("헬로월드")
+                new CustomApnsPayloadBuilder()
+                        .setPostDetails("1")
+                        .setAlertBody("Test Notification!!")
                         .build()
         );
 
@@ -47,7 +45,6 @@ class PushServiceTest {
                 apnsClient.sendNotification(pushNotification).get();
 
         assertTrue(pushNotificationResponse.isAccepted(), "Push notification should be accepted by APNs gateway");
-
         apnsClient.close();
     }
 }
