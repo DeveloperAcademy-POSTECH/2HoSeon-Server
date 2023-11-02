dd7b84555b187b1c6bcc6eb6d8659ffda883b355
Pin <ppyj48625@naver.com>
2023-11-03 01:55:09 +0900

    refactor: 필요없는 사진 조회 endpoint 제거 #20

--- src/main/java/com/twohoseon/app/controller/FileUploaderController.java
+++ src/main/java/com/twohoseon/app/controller/FileUploaderController.java
@@ -7,13 +7,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -74,32 +73,5 @@ public class FileUploaderController {
                 .build();
 
         return ok(responseDTO);
-    }
-
-    @Operation(summary = "프로필 사진 조회")
-    @GetMapping(value = "/profiles/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> searchProfileImage(@PathVariable("imageName") String imageName) throws IOException {
-
-        byte[] imageByteArray = imageService.searchProfileImage(imageName);
-
-        return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
-    }
-
-    @Operation(summary = "포스트 사진 조회")
-    @GetMapping(value = "/posts/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> searchPostImage(@PathVariable("imageName") String imageName) throws IOException {
-
-        byte[] imageByteArray = imageService.searchPostImage(imageName);
-
-        return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
-    }
-
-    @Operation(summary = "후기 사진 조회")
-    @GetMapping(value = "/reviews/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> searchReviewImage(@PathVariable("imageName") String imageName) throws IOException {
-
-        byte[] imageByteArray = imageService.searchReviewImage(imageName);
-
-        return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
     }
 }
