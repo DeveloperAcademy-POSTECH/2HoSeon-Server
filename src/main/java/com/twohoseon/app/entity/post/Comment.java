5eec9e6c83cdd1ed5028f38ae441c4b8c935622f
migusdn <migusdn@gmail.com>
2023-11-03 17:01:38 +0900

    chore: rename PostComment -> Comment

---
+++ src/main/java/com/twohoseon/app/entity/post/Comment.java
@@ -0,0 +1,70 @@
+package com.twohoseon.app.entity.post;
+
+import com.twohoseon.app.common.BaseTimeEntity;
+import com.twohoseon.app.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : hyunwoopark
+ * @version : 1.0.0
+ * @package : twohoseon
+ * @name : Comment
+ * @date : 10/17/23 4:59PM
+ * @modifyed : $
+ **/
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column
+    private String content;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parentComment;
+
+    // 대댓글 리스트
+    @Builder.Default
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> childComments = new ArrayList<>();
+
+    public void updateContent(String postContent) {
+        this.content = postContent;
+    }
+
+    public void updateParent(Comment parentComment) {
+        this.parentComment = parentComment;
+    }
+
+    public void addChildComment(Comment childComment) {
+        this.childComments.add(childComment);
+    }
+
+    public boolean validateMember(Member author) {
+        return !this.author.equals(author);
+    }
+
+}
