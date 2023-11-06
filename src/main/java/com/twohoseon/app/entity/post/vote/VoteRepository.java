7c542d55275bf10b953e3f14d5d5519beba8fdb3
Pin <ppyj48625@naver.com>
2023-11-06 16:56:19 +0900

    refactor: 사용하지 않는 클래스 삭제

--- src/main/java/com/twohoseon/app/entity/post/vote/VoteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.twohoseon.app.entity.post.vote;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<Vote, VoteId> {
-}
