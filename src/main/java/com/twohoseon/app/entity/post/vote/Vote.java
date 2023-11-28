adda9bc42c450a11561784b56d08315d14e99263
migusdn <migusdn@gmail.com>
2023-11-28 23:23:49 +0900

    fix: 중복투표 해결
    요청이 동시에 들어올 때 중복으로 투표가 되던 현상 제약조건 설정

--- src/main/java/com/twohoseon/app/entity/post/vote/Vote.java
+++ src/main/java/com/twohoseon/app/entity/post/vote/Vote.java
@@ -13,6 +13,9 @@ import lombok.Getter;
 @Entity
 @Builder
 @AllArgsConstructor
+@Table(name = "VOTE", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"voter_id", "post_id"})
+})
 public class Vote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
